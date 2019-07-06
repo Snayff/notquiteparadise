ef3054b3ee77da8cb4decab10393b8ff8c6a3c29
Josh Snaith <snaith2@gmail.com>
2019-07-06 17:08:55 +0100

    added get methods for all jsons

--- scripts/core/library_of_alexandria.py
+++ scripts/core/library_of_alexandria.py
@@ -1,4 +1,3 @@
-
 import json
 
 
@@ -6,48 +5,172 @@ class LibraryOfAlexandria:
     """
     The centre of all knowledge in the world.
     """
+
     def __init__(self):
         # TODO -
-        #  override __getitem__ to allow . notation
         #  point all data references here
         #  where we are converting string to enum revert to string use. Enums are internal only. 
 
+        self.skills = {}
+        self.homeland = {}
+        self.race = {}
+        self.savvy = {}
+        self.general = {}
+        self.affliction = {}
+        self.aspect = {}
+        self.terrain = {}
+
+        self.load_data_into_library()
+
+    def load_data_into_library(self):
+        """
+        Load data from all external jsons to this central data library
+        """
         self.skills = self.get_values_from_skill_json()
         self.homeland = self.get_values_from_homeland_json()
         self.race = self.get_values_from_race_json()
         self.savvy = self.get_values_from_savvy_json()
         self.general = self.get_values_from_general_json()
-        self.afflictions = self.get_values_from_affliction_json()
+        self.affliction = self.get_values_from_affliction_json()
         self.aspect = self.get_values_from_aspect_json()
         self.terrain = self.get_values_from_terrain_json()
-
-        self.test = 1
-
-
-    def get_skill(self, skill_tree, skill_name):
-
-        x = self.skills
-        y = "cleromancer"
-        z = "throw_dice"
-
-        from collections import namedtuple
-        named_tuple = namedtuple(z, list(x[y][z]))
-        skill_data = named_tuple(**x[y][z])
-
-        return skill_data
-
-    def get_aspect(self, aspect_string):
+    
+    def get_terrain_data(self, terrain_name):
         """
+        Get data for a terrains from the central library
 
         Args:
-            aspect_string ():
+            terrain_name (str):
 
         Returns:
-            self.aspect:
+            tuple: named tuple of values.
         """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(terrain_name, self.terrain[terrain_name])
+        data = named_tuple(**self.terrain[terrain_name])
 
+        return data
+    
+    def get_aspect_data(self, aspect_name):
+        """
+        Get data for a aspects from the central library
 
-        return self.aspect[aspect_string]
+        Args:
+            aspect_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(aspect_name, self.aspect[aspect_name])
+        data = named_tuple(**self.aspect[aspect_name])
+
+        return data
+    
+    def get_affliction_data(self, affliction_name):
+        """
+        Get data for a afflictions from the central library
+
+        Args:
+            affliction_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(affliction_name, self.affliction[affliction_name])
+        data = named_tuple(**self.affliction[affliction_name])
+
+        return data
+    
+    def get_general_data(self, general_name):
+        """
+        Get data for a general from the central library
+
+        Args:
+            general_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(general_name, self.general[general_name])
+        data = named_tuple(**self.general[general_name])
+
+        return data
+
+    def get_savvy_data(self, savvy_name):
+        """
+        Get data for a savvy from the central library
+
+        Args:
+            savvy_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(savvy_name, self.savvy[savvy_name])
+        data = named_tuple(**self.savvy[savvy_name])
+
+        return data
+
+    def get_race_data(self, race_name):
+        """
+        Get data for a race from the central library
+
+        Args:
+            race_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(race_name, self.race[race_name])
+        data = named_tuple(**self.race[race_name])
+
+        return data
+    
+    def get_homeland_data(self, homeland_name):
+        """
+        Get data for a homeland from the central library
+
+        Args:
+            homeland_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that fact never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(homeland_name, self.homeland[homeland_name])
+        data = named_tuple(**self.homeland[homeland_name])
+
+        return data
+
+    def get_skill_data(self, skill_tree, skill_name):
+        """
+        Get data for a skill from the central library
+
+        Args:
+            skill_tree (str):
+            skill_name (str):
+
+        Returns:
+            tuple: named tuple of values.
+        """
+        # NOTE: I do not know how any of this works.  Let's live in hope that never causes a problem.
+        from collections import namedtuple
+        named_tuple = namedtuple(skill_name, list(self.skills[skill_tree][skill_name]))
+        data = named_tuple(**self.skills[skill_tree][skill_name])
+
+        return data
 
     @staticmethod
     def get_values_from_skill_json():
