e51f9b517c5174822f7bbf5947bb066fd429d349
Zaphyk <zaphyk@live.com>
2020-07-18 14:48:27 -0300

    add test for the TriggeAfflictionsEffect

--- tests/engine/effect_test.py
+++ tests/engine/effect_test.py
@@ -41,20 +41,34 @@ class MockAfflictionMovement(MockAffliction):
 
 class TestEffects:
 
+    @staticmethod
+    def _create_default_entity():
+        components = [Identity('mock_entity'), Position(0, 0)]
+        entity = world.create_entity(components)
+        return entity
+
     def test_trigger_afflictions_effect(self):
         """
         Test for the trigger afflictions effect
         """
-        entity = world.create_entity([])
+        affliction_called = None
+
+        def _trigger_affliction_mock(affliction):
+            nonlocal affliction_called
+            affliction_called = affliction
+
+        world_mock.mock_methods({'apply_affliction': _trigger_affliction_mock})
+        entity = TestEffects._create_default_entity()
 
         mock_affliction_movement = MockAfflictionMovement(entity, entity, 5)
         mock_affliction_damage = MockAfflictionDamage(entity, entity, 5)
         afflictions = Afflictions([mock_affliction_damage, mock_affliction_movement])
         world.add_component(entity, afflictions)
 
-        effect = TriggerAfflictionsEffect(entity, entity, trigger_type=AfflictionTrigger.MOVEMENT, success_effects=[], failure_effects=[])
+        effect = TriggerAfflictionsEffect(entity, entity, AfflictionTrigger.MOVEMENT, [], [])
         effect.evaluate()
 
+        assert affliction_called == mock_affliction_movement
     def test_reduce_skill_cooldown_effect(self):
         """
         Test for the reduce skill cooldown effect
