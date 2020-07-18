3b57ad1c1e2540c07b46ecc85ba42add6aae46d2
Zaphyk <zaphyk@live.com>
2020-07-18 14:47:00 -0300

    add a method to mock to ui component of the engine so the window doesn't pop up on tests

---
+++ tests/mocks/ui_mock.py
@@ -0,0 +1,9 @@
+import sys
+import types
+
+module_name = 'scripts.engine.ui.manager'
+mock_module_name = f'{module_name}_mock'
+mock_module = types.ModuleType(mock_module_name)
+
+sys.modules['scripts.engine.ui.manager'] = mock_module
+sys.modules['scripts.engine.ui.manager'].ui = {}
