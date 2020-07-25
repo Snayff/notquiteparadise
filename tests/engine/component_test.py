9d93cf1e0d790aba2334126adda4b6c1a2f69edb
Zaphyk <zaphyk@live.com>
2020-07-25 01:39:57 -0300

    add unit tests

---
+++ tests/engine/component_test.py
@@ -0,0 +1,30 @@
+from scripts.engine import state
+from scripts.engine.core.constants import GameState
+
+
+class TestState:
+
+
+    def test_position_contains(self):
+        """
+        Test the Position component
+        """
+        pass
+
+    def test_position_contains(self):
+        """
+        Test the Position component
+        """
+        pass
+
+    def test_position_contains(self):
+        """
+        Test the Position component
+        """
+        pass
+
+    def test_position_contains(self):
+        """
+        Test the Position component
+        """
+        pass
