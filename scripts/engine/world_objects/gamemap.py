654e36be4cbb97c108015ef1dcef38639e873758
Zaphyk <zaphyk@live.com>
2020-07-30 01:25:58 -0300

    fix type hinting

--- scripts/engine/world_objects/gamemap.py
+++ scripts/engine/world_objects/gamemap.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from typing import List
-from scripts.engine.world_objects.world_gen import WorldGeneration
+from scripts.engine.world_objects.world_gen import DungeonGeneration
 from scripts.engine.world_objects.tile import Tile
 
 
@@ -10,13 +10,15 @@ class GameMap:
     object to hold tile and fov
     """
     def __init__(self, seed: int, algorithm_name: str, width: int, height: int):
-        self.tiles: List[List[Tile]] = None
-        self.rooms: List[List[List[int]]] = None
+        self.tiles: List[List[Tile]] = []
+        self.rooms: List[List[List[int]]] = []
         self.width = width
         self.height = height
-        self.world_gen = WorldGeneration(seed, algorithm_name, width, height)
+        self.seed = seed
+        self.world_gen = DungeonGeneration(seed, algorithm_name, width, height)
         tiles, rooms = self.world_gen.generate()
         self.tiles = tiles
         self.rooms = rooms
+
 
 
