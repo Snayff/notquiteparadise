e729d647240f4c97806cc07535e865ceb67f1077
Josh Snaith <snaith2@gmail.com>
2020-10-03 23:53:27 +0100

    separated and renamed nqp processors

--- scripts/nqp/processors/intent.py
+++ scripts/nqp/processors/intent.py
@@ -1,109 +1,27 @@
 from __future__ import annotations
 
 import logging
-import os
-from typing import TYPE_CHECKING, Optional, Type
+from typing import Optional, Type
 
-import pygame
-import snecs
 from snecs.typedefs import EntityID
 
-from scripts.engine import chronicle, debug, key, library, state, utility, world
+from scripts.engine import chronicle, debug, library, state, utility, world
 from scripts.engine.action import Skill
-from scripts.engine.component import Aesthetic, IsActor, Knowledge, Position, WinCondition
-from scripts.engine.core import queries
-from scripts.engine.core.constants import (
-    ASSET_PATH,
-    SAVE_PATH,
-    Direction,
-    DirectionType,
-    GameEvent,
-    GameState,
-    GameStateType,
-    InputEvent,
-    InputIntent,
-    InputIntentType,
-    RenderLayer,
-    TargetingMethod,
-    UIElement,
-)
-from scripts.engine.core.data import store
-from scripts.engine.core.definitions import ActorData, TraitSpritePathsData
-from scripts.engine.systems import behaviour, vision
-from scripts.engine.ui.elements.actor_info import ActorInfo
+from scripts.engine.component import Knowledge, Position
+from scripts.engine.core.constants import Direction, DirectionType, GameState, GameStateType, InputIntent, \
+    InputIntentType, \
+    SAVE_PATH, \
+    TargetingMethod, UIElement
+from scripts.engine.systems import behaviour
 from scripts.engine.ui.manager import ui
-from scripts.engine.world_objects.game_map import GameMap
 from scripts.engine.world_objects.tile import Tile
 
-if TYPE_CHECKING:
-    from typing import Optional
-
-
-########################## OUTER LAYER PROCESSORS - EXTERNAL FACING #####################################
-
-
-def process_event(event: pygame.event, game_state: GameStateType):
-    """
-    Extract the intent from the event and process them in the context of the game state. If an event can only be
-    called in one way then no intent is generated and the event will directly go to the relevant action.
-    """
-    intent = None
-    # some events only apply to certain GameStates, we need to process them and separate them
-    if event.type == InputEvent.TILE_CLICK:
-
-        if game_state == GameState.TARGETING:
-            ## Activate skill on mouse click while in targeting mode
-            player = world.get_player()
-            position = world.get_entitys_component(player, Position)
-            if position:
-                event_x, event_y = event.tile_pos
-                direction = (max(-1, min(1, event_x - position.x)), max(-1, min(1, position.y - event_y)))
-                intent = key.convert_vector_to_intent(direction)
-
-        elif game_state == GameState.GAMEMAP:
-            ## Activate Actor Info Menu
-            x, y = event.tile_pos
-            # get entity on tile
-            for entity, (position, _) in queries.position_and_actor:  # type: ignore
-                if (x, y) in position:
-                    # found entity, set to selected
-                    actor_info: ActorInfo = ui.get_element(UIElement.ACTOR_INFO)
-                    actor_info.set_entity(entity)
-                    intent = InputIntent.ACTOR_INFO_TOGGLE
-
-    elif event.type == InputEvent.SKILL_BAR_CLICK:
-        intent = event.skill_intent
-
-    elif event.type == GameEvent.EXIT_MENU:
-        ## Exit Actor Info
-        if event.menu == UIElement.ACTOR_INFO:
-            intent = InputIntent.ACTOR_INFO_TOGGLE
-
-    elif event.type == GameEvent.NEW_GAME:
-        ui.set_element_visibility(UIElement.TITLE_SCREEN, False)
-        _new_game()
-
-    elif event.type == GameEvent.LOAD_GAME:
-        ui.set_element_visibility(UIElement.TITLE_SCREEN, False)
-        _load_game()
-
-    elif event.type == GameEvent.EXIT_GAME:
-        _exit_game()
-
-    elif event.type == GameEvent.WIN_CONDITION_MET:
-        _win_game()
-
-    else:
-        intent = key.convert_to_intent(event)
-
-    # if we have an intent we need to process, do so
-    if intent:
-        process_intent(intent, game_state)
+__all__ = ["process_intent"]
 
 
 def process_intent(intent: InputIntentType, game_state: GameStateType):
     """
-    Process the intent in the context of the game state
+    Process the intent in the context of the game state. Intents are game state sensitive.
     """
     _process_stateless_intents(intent)
 
@@ -113,9 +31,6 @@ def process_intent(intent: InputIntentType, game_state: GameStateType):
         _process_targeting_mode_intents(intent)
     elif game_state == GameState.MENU:
         _process_menu_intents(intent)
-
-
-############################### INNER PROCESSORS - LOCAL ONLY ################################
 
 
 def _process_stateless_intents(intent: InputIntentType):
@@ -304,9 +219,6 @@ def _process_skill_use(player: EntityID, skill: Type[Skill], target_tile: Tile, 
             logging.warning("Process skill use: tried to call camera but not init`d.")
 
 
-######################### GET ##########################
-
-
 def _get_pressed_direction(intent: InputIntentType) -> DirectionType:
     """
     Get the value of the directions pressed. Returns as (x, y). Values are ints between -1 and 1.
@@ -361,132 +273,3 @@ def _get_pressed_skills_name(intent: InputIntentType) -> Optional[str]:
             except IndexError:
                 logging.warning(f"_get_pressed_skills_name: Tried to use skill {intent} but no skill in that slot.")
     return skill_name
-
-
-############### DIRECT ACTIONS ############################
-
-
-def _new_game():
-    """
-    Create a new game
-    """
-    # create clean snecs.world
-    empty_world = snecs.World()
-    world.move_world(empty_world)
-
-    # init and save map
-    game_map = GameMap("cave", 10)
-    store.current_game_map = game_map
-
-    # populate the map
-    player_data = ActorData(
-        key="player",
-        possible_names=["player"],
-        description="a desc",
-        position_offsets=[(0, 0)],
-        trait_names=["shoom", "soft_tops", "dandy"],
-    )
-    game_map.generate_new_map(player_data)
-
-    # init the player
-    player = world.get_player()
-
-    # create win condition and place next to player
-    player_pos = world.get_entitys_component(player, Position)
-    win_x = player_pos.x + 1
-    win_y = player_pos.y
-    components = []
-    components.append(Position((win_x, win_y)))  # lets hope this doesnt spawn in a wall
-    components.append(WinCondition())
-    traits_paths = [TraitSpritePathsData(idle=str(ASSET_PATH / "world/win_flag.png"))]
-    sprites = utility.build_sprites_from_paths(traits_paths)
-    components.append(Aesthetic(sprites.idle, sprites, traits_paths, RenderLayer.ACTOR, (win_x, win_y)))
-    world.create_entity(components)
-
-    # tell places about the player
-    chronicle.set_turn_holder(player)
-
-    # create a god
-    world.create_god("the_small_gods")
-
-    # show the in game screens
-    ui.set_element_visibility(UIElement.CAMERA, True)
-    ui.set_element_visibility(UIElement.MESSAGE_LOG, True)
-    ui.set_element_visibility(UIElement.SKILL_BAR, True)
-
-    # welcome message
-    ui.create_screen_message("Welcome to Not Quite Paradise")
-
-    # FIXME - entities load before camera so they cant get their screen position.
-    #  If ui loads before entities then it fails due to player not existing. Below is a hacky fix.
-    for entity, (aesthetic, position) in world.get_components([Aesthetic, Position]):
-        aesthetic.draw_x, aesthetic.draw_y = (position.x, position.y)
-        aesthetic.target_draw_x = aesthetic.draw_x
-        aesthetic.target_draw_y = aesthetic.draw_y
-
-    # entities load with a blank fov, update them now
-    vision.process_light_map()
-    vision.process_fov()
-    vision.process_tile_visibility()
-
-    # point the camera at the player, now that FOV is updated
-    pos = world.get_entitys_component(player, Position)
-    camera = ui.get_element(UIElement.CAMERA)
-    camera.set_target((pos.x, pos.y), True)
-
-    # loading finished, give player control
-    state.set_new(GameState.GAMEMAP)
-
-    # prompt turn actions
-    chronicle.end_turn(player, 0)
-
-
-def _load_game():
-    """
-    Load existing game state
-    """
-    full_save_path = str(SAVE_PATH)
-    for save_name in os.listdir(full_save_path):
-        save = save_name.replace(".json", "")
-        state.load_game(save)
-
-        # show the in game screens
-        ui.set_element_visibility(UIElement.CAMERA, True)
-        ui.set_element_visibility(UIElement.MESSAGE_LOG, True)
-        ui.set_element_visibility(UIElement.SKILL_BAR, True)
-
-        # welcome message
-        ui.create_screen_message("Welcome back to Not Quite Paradise")
-
-        # loading finished, give player control
-        state.set_new(GameState.GAMEMAP)
-
-
-def _exit_game():
-    """
-    Exit the game
-    """
-    state.set_new(GameState.EXITING)
-
-
-def _quit_to_title():
-    """
-    Quit out of the game back to the title screen
-    """
-    state.set_new(GameState.MENU)
-
-    # hide the in game screens
-    ui.set_element_visibility(UIElement.CAMERA, False)
-    ui.set_element_visibility(UIElement.MESSAGE_LOG, False)
-    ui.set_element_visibility(UIElement.SKILL_BAR, False)
-
-    # show the title screen
-    ui.set_element_visibility(UIElement.TITLE_SCREEN, True)
-
-
-def _win_game():
-    """
-    Trigger the win game actions
-    """
-    ui.create_screen_message("You wonned. Huzzah.")
-    _quit_to_title()
