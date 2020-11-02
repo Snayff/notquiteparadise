2b89c64124ef38daccd806c02f66950b3b16476b
Snayff <23128268+Snayff@users.noreply.github.com>
2020-11-02 21:22:49 +0000

    feature/benchmarking (#134)
    * added benchmarking
    
    * ci lint and test
    
    Co-authored-by: nqp <snayff@users.noreply.github.com>

--- tests/engine/test_utility.py
+++ tests/engine/test_utility.py
@@ -6,123 +6,134 @@ import pytest  # type: ignore
 from scripts.engine import utility
 from scripts.engine.core.constants import IMAGE_NOT_FOUND_PATH, Shape, ShapeType, TILE_SIZE
 
+test_get_image_returns_image_parameters = [
+    (TILE_SIZE, TILE_SIZE),  # normal
+    (-TILE_SIZE, -TILE_SIZE),  # negative
+    (TILE_SIZE * 10, TILE_SIZE * 10),  # large
+    (TILE_SIZE, TILE_SIZE * 2),  # not square
+]
 
-class TestUtility:
-    test_get_image_returns_image_parameters = [
-        (TILE_SIZE, TILE_SIZE),  # normal
-        (-TILE_SIZE, -TILE_SIZE),  # negative
-        (TILE_SIZE * 10, TILE_SIZE * 10),  # large
-        (TILE_SIZE, TILE_SIZE * 2),  # not square
-    ]
 
-    @pytest.mark.parametrize("desired_dimensions", test_get_image_returns_image_parameters)
-    def test_get_image_returns_image(self, desired_dimensions: Tuple[int, int]):
-        img_path = str(IMAGE_NOT_FOUND_PATH)
-        image = utility.get_image(img_path, desired_dimensions)
-        assert isinstance(image, pygame.Surface)
+@pytest.mark.parametrize("desired_dimensions", test_get_image_returns_image_parameters)
+def test_get_image_returns_image(benchmark, desired_dimensions: Tuple[int, int]):
+    img_path = str(IMAGE_NOT_FOUND_PATH)
+    image = benchmark(utility.get_image, img_path, desired_dimensions)
+    assert isinstance(image, pygame.Surface)
 
-    def test_get_image_returns_copy(self):
-        img_path = str(IMAGE_NOT_FOUND_PATH)
-        image = utility.get_image(img_path, (TILE_SIZE, TILE_SIZE))
-        copy_image = utility.get_image(img_path, (TILE_SIZE, TILE_SIZE), True)
-        assert image is not copy_image
 
-    def test_get_images_returns_images(self):
-        img_paths = [str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH)]
-        images = utility.get_images(img_paths, (TILE_SIZE, TILE_SIZE))
+def test_get_image_returns_copy():
+    img_path = str(IMAGE_NOT_FOUND_PATH)
+    image = utility.get_image(img_path, (TILE_SIZE, TILE_SIZE))
+    copy_image = utility.get_image(img_path, (TILE_SIZE, TILE_SIZE), True)
+    assert image is not copy_image
 
-        assert isinstance(images, list)
-        assert len(images) == 3
 
-    def test_flatten_images(self):
-        img_paths = [str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH)]
-        images = utility.get_images(img_paths)
-        image = utility.flatten_images(images)
+def test_get_images_returns_images():
+    img_paths = [str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH)]
+    images = utility.get_images(img_paths, (TILE_SIZE, TILE_SIZE))
 
-        assert isinstance(image, pygame.Surface)
+    assert isinstance(images, list)
+    assert len(images) == 3
 
-    def test_build_sprites_from_paths(self):
-        from scripts.engine.core.definitions import TraitSpritePathsData
-        trait_paths = TraitSpritePathsData(
-            icon=str(IMAGE_NOT_FOUND_PATH),
-            idle=str(IMAGE_NOT_FOUND_PATH),
-            attack=str(IMAGE_NOT_FOUND_PATH),
-            hit=str(IMAGE_NOT_FOUND_PATH),
-            dead=str(IMAGE_NOT_FOUND_PATH),
-            move=str(IMAGE_NOT_FOUND_PATH),
-        )
-        trait_paths2 = TraitSpritePathsData(
-            icon=str(IMAGE_NOT_FOUND_PATH),
-            idle=str(IMAGE_NOT_FOUND_PATH),
-            attack=str(IMAGE_NOT_FOUND_PATH),
-            hit=str(IMAGE_NOT_FOUND_PATH),
-            dead=str(IMAGE_NOT_FOUND_PATH),
-            move=str(IMAGE_NOT_FOUND_PATH),
-        )
 
-        _trait_paths = [trait_paths, trait_paths2]
+def test_flatten_images(benchmark):
+    img_paths = [str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH), str(IMAGE_NOT_FOUND_PATH)]
+    images = utility.get_images(img_paths)
+    image = benchmark(utility.flatten_images, images)
 
-        # this func uses get_images which has already tested diff sizes
-        trait_sprites = utility.build_sprites_from_paths(_trait_paths, (TILE_SIZE, TILE_SIZE))
+    assert isinstance(image, pygame.Surface)
 
-        from scripts.engine.core.definitions import TraitSpritesData
-        assert isinstance(trait_sprites, TraitSpritesData)
 
-    test_clamp_parameters = [
-        (1, 0, 10),  # positive int
-        (-1, -10, 0),  # negative int
-        (1.0, 0.0, 10.0),  # positive float
-        (-1.0, -10.0, 0.0),  # negative float
-    ]
+def test_build_sprites_from_paths(benchmark):
+    from scripts.engine.core.definitions import TraitSpritePathsData
+    trait_paths = TraitSpritePathsData(
+        icon=str(IMAGE_NOT_FOUND_PATH),
+        idle=str(IMAGE_NOT_FOUND_PATH),
+        attack=str(IMAGE_NOT_FOUND_PATH),
+        hit=str(IMAGE_NOT_FOUND_PATH),
+        dead=str(IMAGE_NOT_FOUND_PATH),
+        move=str(IMAGE_NOT_FOUND_PATH),
+    )
+    trait_paths2 = TraitSpritePathsData(
+        icon=str(IMAGE_NOT_FOUND_PATH),
+        idle=str(IMAGE_NOT_FOUND_PATH),
+        attack=str(IMAGE_NOT_FOUND_PATH),
+        hit=str(IMAGE_NOT_FOUND_PATH),
+        dead=str(IMAGE_NOT_FOUND_PATH),
+        move=str(IMAGE_NOT_FOUND_PATH),
+    )
 
-    @pytest.mark.parametrize("value, min_value, max_value", test_clamp_parameters)
-    def test_clamp(self, value: Union[int, float], min_value: Union[int, float], max_value: Union[int, float]):
-        result = utility.clamp(value, min_value, max_value)
+    _trait_paths = [trait_paths, trait_paths2]
 
-        assert result <= max_value
-        assert result >= min_value
+    # this func uses get_images which has already tested diff sizes
+    trait_sprites = benchmark(utility.build_sprites_from_paths, _trait_paths, (TILE_SIZE, TILE_SIZE))
 
-    test_is_close_parameters = [
-        ((1, 1), (1.025, 1.025), 0.05),  # positive int, default delta
-        ((-1, -1), (-1.025, -1.025), 0.05),  # negative int, default delta
-        ((2.0, 2.0), (2.025, 2.025), 0.05),  # positive float, default delta
-        ((-2.0, -2.0), (-2.025, -2.025), 0.05),  # negative float, default delta
-        ((1.0, 1.0), (4.0, 4.0), 8.0)
-    ]
+    from scripts.engine.core.definitions import TraitSpritesData
+    assert isinstance(trait_sprites, TraitSpritesData)
 
-    @pytest.mark.parametrize("current_pos, target_pos, delta", test_is_close_parameters)
-    def test_is_close(self, current_pos: Tuple[float, float], target_pos: Tuple[float, float], delta: float):
-        result = utility.is_close(current_pos, target_pos, delta)
 
-        assert result
+test_clamp_parameters = [
+    (1, 0, 10),  # positive int
+    (-1, -10, 0),  # negative int
+    (1.0, 0.0, 10.0),  # positive float
+    (-1.0, -10.0, 0.0),  # negative float
+]
 
-    test_is_not_close_parameters = [
-        ((1, 1), (2, 2), 0.05),  # positive int, default delta
-        ((-1, -1), (-2, -2), 0.05),  # negative int, default delta
-        ((1.0, 1.0), (2.0, 2.0), 0.05),  # positive float, default delta
-        ((-1.0, -1.0), (-2.0, -2.0), 0.05),  # negative float, default delta
-        ((1.0, 1.0), (10.0, 10.0), 8.0)
-    ]
 
-    @pytest.mark.parametrize("current_pos, target_pos, delta", test_is_not_close_parameters)
-    def test_is_not_close(self, current_pos: Tuple[float, float], target_pos: Tuple[float, float], delta: float):
-        result = utility.is_close(current_pos, target_pos, delta)
+@pytest.mark.parametrize("value, min_value, max_value", test_clamp_parameters)
+def test_clamp(benchmark, value: Union[int, float], min_value: Union[int, float], max_value: Union[int, float]):
+    result = utility.clamp(value, min_value, max_value)
 
-        assert not result
+    assert result <= max_value
+    assert result >= min_value
 
-    test_get_coordinates_from_shape_parameters = [
-        (Shape.SQUARE, 1, (0, 0), [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]),
-        (Shape.CIRCLE, 1, (0, 0), [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]),
-        (Shape.CROSS, 1, (0, 0), [(0, 0), (1, -1), (-1, 1), (-1, -1), (1, 1)]),
-        (Shape.TARGET, 1, (0, 0), [(0, 0)]),
-        (Shape.CONE, 1, (0, 1), [(0, 0), (1, 1), (0, 1), (-1, 1)]),
-        (Shape.CONE, 1, (0, -1), [(0, 0), (1, -1), (0, -1), (-1, -1)]),
-        (Shape.CONE, 1, (1, 0), [(0, 0), (1, -1), (1, 1), (1, 0)]),
-        (Shape.CONE, 1, (-1, 0), [(0, 0), (-1, 1), (-1, 0), (-1, -1)]),
-    ]
 
-    @pytest.mark.parametrize("shape, size, direction, expected", test_get_coordinates_from_shape_parameters)
-    def test_get_coordinates_from_shape(self, shape: ShapeType, size: int, direction: Tuple[int, int],
-            expected: List[Tuple[int, int]]):
-        coordinates = utility.get_coords_from_shape(shape, size, direction)
-        assert set(coordinates) == set(expected)
+test_is_close_parameters = [
+    ((1, 1), (1.025, 1.025), 0.05),  # positive int, default delta
+    ((-1, -1), (-1.025, -1.025), 0.05),  # negative int, default delta
+    ((2.0, 2.0), (2.025, 2.025), 0.05),  # positive float, default delta
+    ((-2.0, -2.0), (-2.025, -2.025), 0.05),  # negative float, default delta
+    ((1.0, 1.0), (4.0, 4.0), 8.0)
+]
+
+
+@pytest.mark.parametrize("current_pos, target_pos, delta", test_is_close_parameters)
+def test_is_close(benchmark, current_pos: Tuple[float, float], target_pos: Tuple[float, float], delta: float):
+    result = utility.is_close(current_pos, target_pos, delta)
+
+    assert result
+
+
+test_is_not_close_parameters = [
+    ((1, 1), (2, 2), 0.05),  # positive int, default delta
+    ((-1, -1), (-2, -2), 0.05),  # negative int, default delta
+    ((1.0, 1.0), (2.0, 2.0), 0.05),  # positive float, default delta
+    ((-1.0, -1.0), (-2.0, -2.0), 0.05),  # negative float, default delta
+    ((1.0, 1.0), (10.0, 10.0), 8.0)
+]
+
+
+@pytest.mark.parametrize("current_pos, target_pos, delta", test_is_not_close_parameters)
+def test_is_not_close(benchmark, current_pos: Tuple[float, float], target_pos: Tuple[float, float], delta: float):
+    result = utility.is_close(current_pos, target_pos, delta)
+
+    assert not result
+
+
+test_get_coordinates_from_shape_parameters = [
+    (Shape.SQUARE, 1, (0, 0), [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]),
+    (Shape.CIRCLE, 1, (0, 0), [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]),
+    (Shape.CROSS, 1, (0, 0), [(0, 0), (1, -1), (-1, 1), (-1, -1), (1, 1)]),
+    (Shape.TARGET, 1, (0, 0), [(0, 0)]),
+    (Shape.CONE, 1, (0, 1), [(0, 0), (1, 1), (0, 1), (-1, 1)]),
+    (Shape.CONE, 1, (0, -1), [(0, 0), (1, -1), (0, -1), (-1, -1)]),
+    (Shape.CONE, 1, (1, 0), [(0, 0), (1, -1), (1, 1), (1, 0)]),
+    (Shape.CONE, 1, (-1, 0), [(0, 0), (-1, 1), (-1, 0), (-1, -1)]),
+]
+
+
+@pytest.mark.parametrize("shape, size, direction, expected", test_get_coordinates_from_shape_parameters)
+def test_get_coordinates_from_shape(benchmark, shape: ShapeType, size: int, direction: Tuple[int, int],
+        expected: List[Tuple[int, int]]):
+    coordinates = utility.get_coords_from_shape(shape, size, direction)
+    assert set(coordinates) == set(expected)
