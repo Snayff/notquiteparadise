5a18437a52de8c03b920eaca83d2ced08df9acff
Josh Snaith <snaith2@gmail.com>
2021-02-02 10:43:43 +0000

    made player optional to dungen

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2019-2020, Josh Snaith'
 author = 'Josh Snaith (Snayff)'
 
 # The short X.Y version
-version = "0.157.0"
+version = "0.157.1"
 
 
 # -- General configuration ---------------------------------------------------
