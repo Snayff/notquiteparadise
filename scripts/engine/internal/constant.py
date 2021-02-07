a630f7927073dc74cce99fd8472caaaef6fa8b34
Josh Snaith <snaith2@gmail.com>
2021-02-07 16:18:18 +0000

    fix incorrect font on init

--- scripts/engine/internal/constant.py
+++ scripts/engine/internal/constant.py
@@ -11,7 +11,7 @@ import tcod
 
 ######################## TOP LEVEL CONSTANTS ######################################
 
-VERSION = "0.158.0"  # DONT FORGET TO UPDATE SPHINX VERSION
+VERSION = "0.162.0"  # DONT FORGET TO UPDATE SPHINX VERSION
 DEBUG_START = False  # Whether to start directly in debug map
 
 MAX_SKILLS = 6
